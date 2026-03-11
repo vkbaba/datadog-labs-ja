6b617e7a61ab01d150903c793c677bbcd5f8ca47
vkbaba <kota.baba@datadoghq.com>
2026-03-11 17:02:11 +0900

    fix: migrate to astro5

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,18 @@ import rehypeExternalLinks from 'rehype-external-links';
 export default defineConfig({
   site: 'https://vkbaba.github.io',
   base: '/datadog-labs-ja',
+  vite: {
+    ssr: {
+      noExternal: ['zod'],
+    },
+  },
   markdown: {
     rehypePlugins: [
       [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }],
     ],
   },
   integrations: [
+    { name: '@astrojs/sitemap', hooks: {} },
     starlight({
       title: 'Datadog Labs (JP)',
       customCss: ['./src/styles/custom.css'],
@@ -29,11 +35,11 @@ export default defineConfig({
         {
           label: 'SRE 基礎 - 基本から自動修復まで',
           items: [
-            { label: 'コース概要', slug: 'dd-gcp-sre-fundamentals' },
-            { label: '第1章: ラボ環境の確認', slug: 'dd-gcp-sre-fundamentals/01-introduction' },
-            { label: '第2章: SLO の作成', slug: 'dd-gcp-sre-fundamentals/02-creating-an-slo' },
-            { label: '第3章: 環境の分析', slug: 'dd-gcp-sre-fundamentals/03-analyzing-the-environment' },
-            { label: '第4章: まとめ - 自動ワークフローの確認', slug: 'dd-gcp-sre-fundamentals/04-automation-workflow' },
+            { label: 'コース概要', link: '/dd-gcp-sre-fundamentals/' },
+            { label: '第1章: ラボ環境の確認', link: '/dd-gcp-sre-fundamentals/01-introduction/' },
+            { label: '第2章: SLO の作成', link: '/dd-gcp-sre-fundamentals/02-creating-an-slo/' },
+            { label: '第3章: 環境の分析', link: '/dd-gcp-sre-fundamentals/03-analyzing-the-environment/' },
+            { label: '第4章: まとめ - 自動ワークフローの確認', link: '/dd-gcp-sre-fundamentals/04-automation-workflow/' },
           ],
         },
       ],
